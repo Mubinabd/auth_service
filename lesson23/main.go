002fad9c40575c0af22b30e7b9a61be4b3b32ad3
husanmusa <husanmusa@gmail.com>
2024-04-26 16:58:15 +0500

    checking

--- lesson23/main.go
+++ lesson23/main.go
@@ -5,3 +5,6 @@ import "fmt"
 func main() {
 	fmt.Println("BASE")
 }
+
+// localda uchunchi branch yaratib, o'zgarish ni push qilib, pull request yasab,
+// uni merge qilib, yangi branchni githubdan ochirib tashlash
