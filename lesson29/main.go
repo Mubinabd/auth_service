92eb3c194014ecf011ee1852ef08ec2975bb9d42
axadjonovsardorbek <axadjonovsardorbeck@gmail.com>
2024-05-06 18:36:48 +0500

    added lesson39

---
+++ lesson29/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+
+	"gorm.io/driver/postgres"
+)
+
+const (
+	host     = "localhost"
+	user     = "sardorbek"
+	dbname   = "postgres"
+	password = "1111"
+	port     = 5432
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+type Book struct {
+	Id        int
+	Name      string
+	Page      int
+	StudentId int
+}
+
+// func (Book) TableName() string {
+// 	return "book"
+// }
+
+func main() {
+
+	dbCon := fmt.Sprintf("host=%s user=%s dbname=%s password=%s port=%d sslmode=disable",
+		host, user, dbname, password, port)
+
+	db, err := gorm.Open(postgres.Open(dbCon), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// err = db.AutoMigrate(&Product{})
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// db.Create(&Product{Code: "D42", Price: 100})
+
+	var product Product
+	// select * from products where id = 1 order by id limit 1
+	db.First(&product, "code = ? and price = ?", "D42", 100) // find product with integer primary key
+	fmt.Println(product)
+
+	var b Book
+
+	// select * from products where id = 1 limit 1
+	// db.Take(&b)
+	// fmt.Println(b)
+
+	// db = db.Last(&b, "fds= ?", 43)
+	// if db.Error != nil {
+	// 	panic(db.Error)
+	// }
+
+	// fmt.Println(b, res.Error)
+	// var res []Book
+
+	// db = db.Where(&Book{Name: "efhuef", Page: 103}).Find(&res)
+	// if db.Error != nil {
+	// 	panic(db.Error)
+	// }
+
+	// fmt.Println(res)
+
+	db.Where(&Book{ Page: 288}).Model(&b).Updates(&Book{Name: "efhuef", Page: 103})
+
+	db.Delete(&Book{}, 3)
+}
